6d5d2635a2ac2e5c724e4871e28c3799a39c2090
Ronson Wuniardy <rwuniard@hotmail.com>
2020-07-31 22:11:11 -0400

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+    // Commented out these lines since it will fail.
+    // It will try to render fib and fib will call back-end and it will fail.
+    // const div = document.createElement('div');
+    // ReactDOM.render(<App />, div);
+    // ReactDOM.unmountComponentAtNode(div);
+});
